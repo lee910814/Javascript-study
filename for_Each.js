8eae11b533fac5f9f5f66b0feea99ff702558db8
DESKTOP-0N06855\user <wantto13@naver.com>
2025-09-23 23:42:45 +0900

    for_Each문 공부

---
+++ for_Each.js
@@ -0,0 +1,11 @@
+const students = ["ANNY","MINSU","MINI","SANA"]
+
+students.forEach(function(student){
+    document.write(`${student}.`)
+});
+
+const names = ["Park","kim","lee","gil"]
+
+names.forEach(function(name){
+    document.write(`${name}`)
+})
